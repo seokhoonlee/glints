939849d86a609b9eb23ea3ce96e734791c9c07b9
seokhoonlee <seokhoonlee9393@gmail.com>
2019-01-15 00:38:55 +0800

    Print date with ordinals and full months spelled

--- XuTheGreat.java
+++ XuTheGreat.java
@@ -66,9 +66,58 @@ public class XuTheGreat {
       this.day = day;
     }
 
+    private String printDay(int day) {
+      if ((1 <= day && day <= 10) || (21 <= day && day <= 31)) {
+        if (day % 10 == 1) {
+          return day + "st";
+        } else if (day % 10 == 2) {
+          return day + "nd";
+        } else if (day % 10 == 3) {
+          return day + "rd";
+        } else {
+          return day + "th";
+        }
+      } else if (11 <= day && day <= 20) {
+        return day + "th";
+      } else {
+        return "DAY NOT IN RANGE";
+      }
+    }
+
+    private String printMonth(int month) {
+      switch (month) {
+        case 1:
+          return "January";
+        case 2:
+          return "February";
+        case 3:
+          return "March";
+        case 4:
+          return "April";
+        case 5:
+          return "May";
+        case 6:
+          return "June";
+        case 7:
+          return "July";
+        case 8:
+          return "August";
+        case 9:
+          return "September";
+        case 10:
+          return "October";
+        case 11:
+          return "November";
+        case 12:
+          return "December";
+        default:
+          return "MONTH NOT IN RANGE";
+      }
+    }
+
     @Override
     public String toString() {
-      return day + " " + month + " " + year;
+      return this.printDay(this.day) + " " + this.printMonth(this.month) + " " + this.year;
     }
   }
 
