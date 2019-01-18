e7b428000723fa7a9321c67a1630421ac9dbe5b5
seokhoonlee <seokhoonlee9393@gmail.com>
2019-01-18 23:08:34 +0800

    Print out pairs

--- Task 3/TalentHunt.java
+++ Task 3/TalentHunt.java
@@ -9,13 +9,13 @@ public class TalentHunt {
     private Integer jobIndex;
 
     public Pair(Integer candidateIndex, Integer jobIndex) {
-      this.candidateIndex = candidateIndex + 1; // account for difference between zero index and one idnex
-      this.jobIndex = jobIndex + 1; // account for difference between zero index and one idnex
+      this.candidateIndex = candidateIndex; // account for difference between zero index and one idnex
+      this.jobIndex = jobIndex; // account for difference between zero index and one idnex
     }
 
     @Override
     public String toString() {
-      return "c" + candidateIndex + " j" + jobIndex;
+      return "c" + (candidateIndex + 1) + " j" + (jobIndex + 1);
     }
 
     // @Override
@@ -47,16 +47,14 @@ public class TalentHunt {
   }
 
   public void generateMatrix(List<String> inputLines) {
-    int[][] jobMatrix = new int[inputLines.size() / 2][inputLines.size() / 2];
-    int[][] candidateMatrix = new int[inputLines.size() / 2][inputLines.size() / 2];
+    jobMatrix = new int[inputLines.size() / 2][inputLines.size() / 2];
+    candidateMatrix = new int[inputLines.size() / 2][inputLines.size() / 2];
 
     int lineCount = 0;
 
     for (String line: inputLines) {
       line = line.replaceAll("[jc:]","");
       String[] lineWords = line.split(" ");
-
-      System.out.println(line);
 
       if (lineCount < inputLines.size() / 2) {
         int jobIndex = Integer.valueOf(lineWords[0]);
@@ -73,7 +71,92 @@ public class TalentHunt {
       }
 
       lineCount++;
-    }   
+    }
+
+    // for (int i = 0; i < inputLines.size() / 2; i++) {
+    //   for (int j = 0; j < inputLines.size() / 2; j++) {
+    //     System.out.print(jobMatrix[i][j] + " ");
+    //   }
+    //   System.out.println();
+    // }
+
+    // for (int i = 0; i < inputLines.size() / 2; i++) {
+    //   for (int j = 0; j < inputLines.size() / 2; j++) {
+    //     System.out.print(candidateMatrix[i][j] + " ");
+    //   }
+    //   System.out.println();
+    // }
+  }
+
+  public List<Pair> matchJobAndCadidate() {
+    int[] candidateChoices = new int[candidateMatrix.length];
+    Set<Integer> chosenJob = new HashSet<>();
+    int numChosenJob = 0;
+
+    for (int i = 0; i < candidateMatrix.length; i++) {
+      int firstChoiceJob = candidateMatrix[i][0];
+
+      if (!chosenJob.contains(firstChoiceJob)) {
+        chosenJob.add(firstChoiceJob);
+        numChosenJob++;
+      }
+
+      candidateChoices[i] = firstChoiceJob;
+    }
+
+    while (numChosenJob != jobMatrix.length) {
+      for (int i = 0; i < candidateChoices.length; i++) {
+        for (int j = i + 1; j < candidateChoices.length; j++) {
+          if (candidateChoices[i] == candidateChoices[j]) {
+            int jobsRankingForI = getRankingForCandidate(jobMatrix[candidateChoices[i]], i);
+            int jobsRankingForJ = getRankingForCandidate(jobMatrix[candidateChoices[j]], j);
+
+            int newChoiceJob;
+
+            if (jobsRankingForI < jobsRankingForJ) {
+              newChoiceJob = getNextChoiceJob(candidateMatrix[j], candidateChoices[j]);
+              candidateChoices[j] = newChoiceJob;
+            } else {
+              newChoiceJob = getNextChoiceJob(candidateMatrix[i], candidateChoices[i]);
+              candidateChoices[i] = newChoiceJob;
+            }
+
+            if (!chosenJob.contains(newChoiceJob)) {
+              chosenJob.add(newChoiceJob);
+              numChosenJob++;
+            }
+          }
+        }
+      }
+    }
+
+    List<Pair> pairs = new ArrayList<>();
+
+    for (int i = 0; i < candidateChoices.length; i++) {
+      pairs.add(new Pair(i, candidateChoices[i]));
+    }
+
+    return pairs;
+  }
+
+  public int getNextChoiceJob(int[] candidateRankingForJob, int candidateOldChoice) {
+    for (int i = 0; i < candidateRankingForJob.length - 1; i++) {
+      if (candidateRankingForJob[i] == candidateOldChoice) {
+        return candidateRankingForJob[i + 1];
+      }
+    }
+
+    return candidateRankingForJob[candidateRankingForJob.length - 1];
+  }
+
+  public int getRankingForCandidate(int[] jobRankingForCandidate, int candidateIndex) {
+    for(int i = 0; i < jobRankingForCandidate.length; i++) {
+      if (jobRankingForCandidate[i] == candidateIndex) {
+        return i;
+      }
+    }
+
+    return -1;
   }
 
   public static void main(String[] args) {
@@ -89,10 +172,10 @@ public class TalentHunt {
 
     talentHunt.generateMatrix(inputLines);
 
-    // List<Pair> pairs = talentHunt.match();
+    List<Pair> pairs = talentHunt.matchJobAndCadidate();
 
     // Collections.sort(pairs);
 
-    // talentHunt.printPairs(pairs);
+    talentHunt.printPairs(pairs);
   }  
 }
